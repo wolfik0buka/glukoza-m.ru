d61548efe0e247666a64e95aa560d0c521b3bfff
root <root@329946-glukoza.tmweb.ru>
2020-11-20 14:17:52 +0000

    fix bug with images

--- laravel/config/filesystems.php
+++ laravel/config/filesystems.php
@@ -45,7 +45,8 @@ return [
 
 		'local' => [
 			'driver' => 'local',
-			'root'   => storage_path().'/app',
+			//'root'   => storage_path().'/app',
+			'root'   => "/var/www/cdn/glukoza-med.ru"
 		],
 
         'selectel' => [
