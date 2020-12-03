7fde727a1a6c511d219ede9acedddf6b064787d1
wolfik <molodoi_padavan@mail.ru>
2020-12-03 17:22:32 +0300

    Добавлен 3-й уровень меню на сайте

--- srcVUE/components/ExpandCatSecond.vue
+++ srcVUE/components/ExpandCatSecond.vue
@@ -1,39 +1,36 @@
 <template>
-    <div>
-        <a
-            @mouseover="setActive"
-            :class="{ active: isActive }"
-            :href="$store.state.catUrl + cat.slug"
+    <div class="expandCat__level2_wrap">
+        <a :class="{ active: isActiveS }" :href="$store.state.catUrl + cat.slug"
             >{{ cat.name
             }}<i v-if="isIssetChilds & !isMobile" class="fa fa-angle-right"></i
         ></a>
 
         <div
-            v-if="isActive && !isMobile && isIssetChilds"
-            class="expandCat__level2 child_cat"
+            v-if="!isMobile && isIssetChilds"
+            class="expandCat__level2 expandCat__level3 child_cat"
         >
             <div class="cat_title">{{ cat.name }}</div>
-            <expand-cat-second
+            <a
                 v-for="child in childs"
-                :isMobile="isMobile"
-                :cat="child"
+                :href="$store.state.catUrl + child.slug"
+                :key="child.id"
+                >{{ child.name }}</a
             >
-            </expand-cat-second>
         </div>
     </div>
 </template>
 <script>
 module.exports = {
-    name: "ExpandCatItem",
+    name: "ExpandCatSecond",
     props: ["cat", "isMobile"],
     data() {
         return {
-            empty: false,
+            componentKey: 0,
         };
     },
     computed: {
-        isActive() {
-            return this.$store.state.catLevelOneActive == this.cat.id;
+        isActiveS() {
+            return this.$store.state.catLevelSecondActive == this.cat.id;
         },
         childs() {
             let _res = _(this.$store.state.catalog)
@@ -50,13 +47,6 @@ module.exports = {
             return this.childs.length > 0;
         },
     },
-    methods: {
-        setActive() {
-            this.$store.commit("setExpandMenuActiveCat", this.cat.id);
-        },
-    },
-    components: {
-        ExpandCatSecond: require("./ExpandCatSecond.vue").default,
-    },
+    methods: {},
 };
 </script>
